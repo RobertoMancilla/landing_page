9c4eff7a8cba0422914c5c3844a5bfe50ea3f228
RobertoMancilla <mancillarober24@gmail.com>
2025-09-25 18:49:44 -0600

    cambio agregando analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
